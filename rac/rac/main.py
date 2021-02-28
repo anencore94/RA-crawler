56609abc507b80fc3b0385bff8ec668098c42e46
anencore94 <anencore94@gmail.com>
2021-02-28 15:59:54 +0900

    Bugfix user can enter keywords with parameters

--- rac/rac/main.py
+++ rac/rac/main.py
@@ -9,7 +9,8 @@ import naver
 
 def run(param):
     t_bot = telegram.Bot(token=param['bot_access_token'])
-
+    key = param['keywords_to_search']
+    print(f'키워드 {key}에 속하는 채용 공고를 찾습니다.')
     msg_candidates = [line.line(param['keywords_to_search']),
                       kakao.kakao(param['keywords_to_search']),
                       naver.naver(param['keywords_to_search'])]
@@ -25,17 +26,12 @@ def run(param):
 if __name__ == "__main__":
     bot_access_token = '1638721240:AAGUo6cYy0VujwOJEcvZ9hRu_mee3aTFhpM'
     my_telegram_id = '1010631797'
-    keywords_to_search = ['m/l', 'd/l', 'ml', 'dl', 'machine', 'deep',
-                          'scientist',
-                          '머신', '딥', '사이언티스트', '사이언스', '추천', '학습',
-                          '전문연구']
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--bot_access_token', type=str,
                         default=bot_access_token)
     parser.add_argument('--telegram_id', type=str, default=my_telegram_id)
-    parser.add_argument('--keywords_to_search', type=list,
-                        default=keywords_to_search)
+    parser.add_argument('--keywords_to_search', '-k', action='append')
 
     args = parser.parse_args()
     dict_formatted_args = vars(args)
